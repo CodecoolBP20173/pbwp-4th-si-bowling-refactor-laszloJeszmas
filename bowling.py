f678a2944130235a3be277af7955465ac7adfcfe
Laszlo Jeszmas <rolne1992@gmail.com>
2017-11-25 15:31:16 +0100

    Put in more get_value function. The calculation on the next one and two throws part is shorter now.

--- bowling.py
+++ bowling.py
@@ -2,31 +2,27 @@ def score(game):
     #print(game)
     result = 0
     frame = 1
-    in_first_half = True
+    in_first_half = True # I should understan what is this.
     for throw in range(len(game)):
         if game[throw] == '/':
-            result += 10 - last
+            result += get_value(game[throw]) - last
         else:
             result += get_value(game[throw])
         if frame < 10 and get_value(game[throw]) == 10:
             if game[throw] == '/':
                 result += get_value(game[throw+1])
             elif game[throw].lower() == 'x':
-                result += get_value(game[throw+1])
-                if game[throw+2] == '/':
-                    result += 10 - get_value(game[throw+1])
-                else:
-                    result += get_value(game[throw+2])
-        last = get_value(game[throw])
-        if not in_first_half:
+                if game[throw+2] != '/':
+                    result += get_value(game[throw+1])
+                result +=  get_value(game[throw+2])
+
+        if in_first_half == False:
             frame += 1
-        if in_first_half == True:
-            in_first_half = False
-        else:
-            in_first_half = True
+        in_first_half = not in_first_half
         if game[throw].lower() == 'x':
             in_first_half = True
             frame += 1
+        last = get_value(game[throw])
     return result
 
 
