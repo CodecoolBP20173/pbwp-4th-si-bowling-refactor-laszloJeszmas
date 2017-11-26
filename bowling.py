90d2b2ca5b21ae225dd20d9429d64af6db51ca9a
Laszlo Jeszmas <rolne1992@gmail.com>
2017-11-26 14:26:17 +0100

    Changed the order of the functions. Renamed "first_half" function to "frame_counter", because it describes it's function way more precisly.

--- bowling.py
+++ bowling.py
@@ -1,28 +1,14 @@
 def score(game):
     result = 0
     frame = 1
-    in_first_half = True  # I should understan what is this.
+    in_first_half = True
     for throw in range(len(game)):
         result += get_value(game[throw])
         if game[throw] == '/':
             result -= last
         result = bonus_score(result, frame, game, throw)
-        frame, in_first_half = first_half(in_first_half, frame, game, throw)
+        frame, in_first_half = frame_counter(in_first_half, frame, game, throw)
         last = get_value(game[throw])
-    return result
-
-def bonus_score(result, frame, game, throw):
-    MAX_POINT = 10
-    LAST_FRAME = 10
-    NEXT_THROW = throw+1
-    AFTER_NEXT_THROW = throw + 2
-    if frame < LAST_FRAME and get_value(game[throw]) == MAX_POINT:
-        if game[throw] == '/':
-            result += get_value(game[NEXT_THROW])
-        elif game[throw].lower() == 'x':
-            if game[throw+2] != '/':
-                result += get_value(game[NEXT_THROW])
-            result += get_value(game[AFTER_NEXT_THROW])
     return result
 
 
@@ -37,7 +23,22 @@ def get_value(char):
         raise ValueError()
 
 
-def first_half(in_first_half, frame, game, throw):
+def bonus_score(result, frame, game, throw):
+    MAX_POINT = 10
+    LAST_FRAME = 10
+    NEXT_THROW = throw+1
+    AFTER_NEXT_THROW = throw + 2
+    if frame < LAST_FRAME and get_value(game[throw]) == MAX_POINT:
+        if game[throw] == '/':
+            result += get_value(game[NEXT_THROW])
+        elif game[throw].lower() == 'x':
+            if game[AFTER_NEXT_THROW] != '/':
+                result += get_value(game[NEXT_THROW])
+            result += get_value(game[AFTER_NEXT_THROW])
+    return result
+
+
+def frame_counter(in_first_half, frame, game, throw):
         if in_first_half is False:
             frame += 1
         in_first_half = not in_first_half
