914aa769bbe00da0a59d169570de8c6696f50a7d
Laszlo Jeszmas <rolne1992@gmail.com>
2017-11-26 13:58:09 +0100

    The part in 'score' function, which changed the 'frame' variable, now has an own function.

--- bowling.py
+++ bowling.py
@@ -1,27 +1,29 @@
+def first_half(in_first_half, frame, game, throw):
+        if in_first_half is False:
+            frame += 1
+        in_first_half = not in_first_half
+        if game[throw].lower() == 'x':
+            in_first_half = True
+            frame += 1
+        return frame, in_first_half
+
+
 def score(game):
-    #print(game)
     result = 0
     frame = 1
-    in_first_half = True # I should understan what is this.
+    in_first_half = True  # I should understan what is this.
     for throw in range(len(game)):
+        result += get_value(game[throw])
         if game[throw] == '/':
-            result += get_value(game[throw]) - last
-        else:
-            result += get_value(game[throw])
+            result -= last
         if frame < 10 and get_value(game[throw]) == 10:
             if game[throw] == '/':
                 result += get_value(game[throw+1])
             elif game[throw].lower() == 'x':
                 if game[throw+2] != '/':
                     result += get_value(game[throw+1])
-                result +=  get_value(game[throw+2])
-
-        if in_first_half == False:
-            frame += 1
-        in_first_half = not in_first_half
-        if game[throw].lower() == 'x':
-            in_first_half = True
-            frame += 1
+                result += get_value(game[throw+2])
+        frame, in_first_half = first_half(in_first_half, frame, game, throw)
         last = get_value(game[throw])
     return result
 
@@ -29,7 +31,7 @@ def score(game):
 def get_value(char):
     if char in map(str, range(1, 10)):
         return int(char)
-    elif char.lower() == 'x' or char == '/': #Is it sure that it won't ever get an error if it want to lower non-alphabetical symbols?
+    elif char.lower() == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
