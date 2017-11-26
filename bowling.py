99c37e30097fb7550308f6a5a0f8936b7515a81b
Laszlo Jeszmas <rolne1992@gmail.com>
2017-11-26 14:13:14 +0100

    Now the extra points are calculated in a different function, not in 'score'.

--- bowling.py
+++ bowling.py
@@ -1,24 +1,28 @@
 def score(game):
-    MAX_POINT = 10
-    LAST_FRAME = 10
     result = 0
     frame = 1
     in_first_half = True  # I should understan what is this.
     for throw in range(len(game)):
-        NEXT_THROW = throw+1
-        AFTER_NEXT_THROW = throw + 2
         result += get_value(game[throw])
         if game[throw] == '/':
             result -= last
-        if frame < LAST_FRAME and get_value(game[throw]) == MAX_POINT:
-            if game[throw] == '/':
-                result += get_value(game[NEXT_THROW])
-            elif game[throw].lower() == 'x':
-                if game[AFTER_NEXT_THROW] != '/':
-                    result += get_value(game[NEXT_THROW])
-                result += get_value(game[AFTER_NEXT_THROW])
+        result = bonus_score(result, frame, game, throw)
         frame, in_first_half = first_half(in_first_half, frame, game, throw)
         last = get_value(game[throw])
+    return result
+
+def bonus_score(result, frame, game, throw):
+    MAX_POINT = 10
+    LAST_FRAME = 10
+    NEXT_THROW = throw+1
+    AFTER_NEXT_THROW = throw + 2
+    if frame < LAST_FRAME and get_value(game[throw]) == MAX_POINT:
+        if game[throw] == '/':
+            result += get_value(game[NEXT_THROW])
+        elif game[throw].lower() == 'x':
+            if game[throw+2] != '/':
+                result += get_value(game[NEXT_THROW])
+            result += get_value(game[AFTER_NEXT_THROW])
     return result
 
 
