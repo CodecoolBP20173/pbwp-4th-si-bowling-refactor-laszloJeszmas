82a5b6d732807b769ace34b6f8909146938eb0c1
Laszlo Jeszmas <rolne1992@gmail.com>
2017-11-26 14:01:07 +0100

    Made MAX_POINT and LAST_FRAME variable, which never changes it's own value.

--- bowling.py
+++ bowling.py
@@ -1,14 +1,6 @@
-def first_half(in_first_half, frame, game, throw):
-        if in_first_half is False:
-            frame += 1
-        in_first_half = not in_first_half
-        if game[throw].lower() == 'x':
-            in_first_half = True
-            frame += 1
-        return frame, in_first_half
-
-
 def score(game):
+    MAX_POINT = 10
+    LAST_FRAME = 10
     result = 0
     frame = 1
     in_first_half = True  # I should understan what is this.
@@ -16,7 +8,7 @@ def score(game):
         result += get_value(game[throw])
         if game[throw] == '/':
             result -= last
-        if frame < 10 and get_value(game[throw]) == 10:
+        if frame < LAST_FRAME and get_value(game[throw]) == MAX_POINT:
             if game[throw] == '/':
                 result += get_value(game[throw+1])
             elif game[throw].lower() == 'x':
@@ -37,3 +29,13 @@ def get_value(char):
         return 0
     else:
         raise ValueError()
+
+
+def first_half(in_first_half, frame, game, throw):
+        if in_first_half is False:
+            frame += 1
+        in_first_half = not in_first_half
+        if game[throw].lower() == 'x':
+            in_first_half = True
+            frame += 1
+        return frame, in_first_half
